23cb551ba6d07c1a8726a5d99cc5d3a1bc1fc555
pallavi002 <pallavilodhi08@gmail.com>
2020-03-06 19:46:15 +0530

    edu details

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -3,9 +3,26 @@ const Profile = require('../models/Profile');
 module.exports.updateprofile = async function (req, res) {
   console.log(req.body);
   try {
-    educationDetails = req.body.educationDetails;
+    let educationDetails = req.body.educationDetails;
+    let goals = req.body.goals;
     let profile = await Profile.findOneAndUpdate({ 'userId': req.body.userId },
-      { $push:  {educationDetails:{ $each: {educationDetails} } } },
+      { $set: {
+          $push: {
+            educationDetails:{ 
+              $each: {
+                educationDetails
+              } 
+            } 
+          },
+          $push: {
+            goals: {
+              $each: {
+                goals
+              }
+            }
+          } 
+        }
+      },
       { 'upsert': true, 'new': true, 'multi': true });
     res.json(profile);
   } catch (err) {
