db54f513e68d505e63ff131c5d7510a9500d8268
pallavi002 <pallavilodhi08@gmail.com>
2020-03-06 14:35:51 +0530

    register

--- controllers/staticController.js
+++ controllers/staticController.js
@@ -49,3 +49,49 @@ module.exports.login = function(req, res){
       })
   }
 }
+
+module.exports.register = function (req, res) {
+  if( req.body.name && req.body.email && req.body.contact) {
+    try {
+      let user = await User.findOne({ 'email': req.body.email });
+      if (!user) {
+        user = new User({
+					email: req.body.email,
+					name: req.body.name,
+          contact: req.body.contact,
+          password: req.body.password,
+					role: req.body.role
+				});
+				User.create(user)
+            .then(user => {
+                // console.log(user);
+                let clientUserData = {
+                    name: user.name,
+                    _id: user._id,
+                    email: user.email,
+                    contact: user.contact,
+                }
+                jwt.sign(clientUserData, process.env.JWT_SECRET_KEY, {expiresIn: '1h'}, (tokenError, token)=> {
+                    if(tokenError){
+                        // console.log(tokenError);
+                        res.json({
+                            status: 204,
+                            error: tokenError
+                        })
+                    }else{
+                        res.json({
+                            message: "Successfully registered",
+                            user: clientUserData,
+                            token
+                        })
+                    }
+                })
+            })
+      }
+    } catch (err) {
+			console.log(err);
+			req.flash('toastMessage', 'Some error. Try again');
+			res.redirect('back');	
+		}
+  }
+}
